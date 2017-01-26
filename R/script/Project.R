70a39cc801e599db8e051d92fd883f67d3cbb587
brmschipper <brmschipper@gmail.com>
2017-01-26 08:47:41 +0100

    joe

--- R/script/Project.R
+++ R/script/Project.R
@@ -1,9 +1,12 @@
+#install.packages("rgeos")
+#install.packages("rgdal")
+
 library(raster)
 library(rgeos)
 library(rgdal)
 
-setwd('C:/Users/Bas/geoscripting/R')
 
+setwd("/home/bram/Documenten/Project/R/")
 boundaries1 <- readOGR("data/CA_counties.shp")
 
 boundaries2 <- readOGR("data/tl_2012_04_cousub.shp")
@@ -15,18 +18,19 @@ boundaries3 <- spTransform(boundaries3, proj4string(boundaries1))
 cities <- readOGR("data/tl_2013_06_place.shp")
 cities <- spTransform(cities, proj4string(boundaries1))
 
-project_extent <- extent(-120, -114, 32.5, 38.5)
+project_extent <- extent(-123, -119.5, 34.9, 40)
 plot(project_extent, col = "darkblue")
 
 boundaries_1_crop <- crop(boundaries1, project_extent)
 plot(boundaries_1_crop, add = TRUE, col = "darkgrey")
-boundaries_2_crop <- crop(boundaries2, project_extent)
-plot(boundaries_2_crop, add = TRUE, col = "lightgrey")
-boundaries_3_crop <- crop(boundaries3, project_extent)
-plot(boundaries_3_crop, add = TRUE, col = "lightgrey")
-cities_crop <- crop(cities, project_extent)
-plot(cities_crop, add = TRUE, col = "black")
-
+plot(spdf, add= TRUE, col="red")
+#boundaries_2_crop <- crop(boundaries2, project_extent)
+#plot(boundaries_2_crop, add = TRUE, col = "lightgrey")
+#boundaries_3_crop <- crop(boundaries3, project_extent)
+#plot(boundaries_3_crop, add = TRUE, col = "lightgrey")
+#cities_crop <- crop(cities, project_extent)
+#(cities_crop, add = TRUE, col = "black")
+plot(spdf, add= TRUE, col="red")
 
 
 
