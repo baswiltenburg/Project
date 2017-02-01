256a3701448c36bc77e835f8a2420e5a418c5e80
baswiltenburg <baswiltenburg@gmail.com>
2017-02-01 09:05:55 +0100

    1 february

--- R/shiny.R
+++ R/shiny.R
@@ -7,7 +7,7 @@ Interpolate_pollution <- function(){
   coordinates <- cbind(firedates$SITE_LONGITUDE, firedates$SITE_LATITUDE)
   
   # Define project extent
-  project_extent <- extent(-123, -117.5, 34.9, 40)
+  project_extent <- extent(-125, -114, 32, 43)
   #plot(project_extent, axes = TRUE)
   
   #Create empty raster
@@ -27,16 +27,15 @@ Interpolate_pollution <- function(){
   #lines(coords, col="red")
   sp_poly <- SpatialPolygons(list(Polygons(list(Polygon(coords)), ID=1)), proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
   sp_poly_df <- SpatialPolygonsDataFrame(sp_poly, data=data.frame(ID=1))
-  
+  USA <- getData('GADM', country='USA', level=1)
+  california <- USA[USA$NAME_1 == "California",]
+  california <- spTransform(california, CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
   df <- spdf_crop@data
   write.csv(df, file = "Dataframes/df_fire_period.csv") 
   
   system("python Python/create_dataframes.py")
   
-  # USA <- getData('GADM', country='USA', level=1)
-  # california <- USA[USA$NAME_1 == "California",]
-  # california <- spTransform(california, CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
-  
+  rasters <- stack()
   for(i in 0:81) {
     filename <- paste("Dataframes/day_",i,".csv",sep="")
     data_per_day <- read.csv(filename)
@@ -44,64 +43,80 @@ Interpolate_pollution <- function(){
     fd_spdf <- SpatialPointsDataFrame(coords = coordinates_fd, data = data_per_day, proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
     idw_cal <- idw(fd_spdf$Daily.Mean.PM2.5.Concentration ~ 1, fd_spdf, grd, idp=6)
     idw_cal_r <- raster(idw_cal)
-    idw_cal_r_m <- mask(idw_cal_r, sp_poly_df, inverse = FALSE)
-    plot(idw_cal_r_m, axes=TRUE)
-    plot(sp_poly_df, add = TRUE)
-    plot(spdf_crop, pch=19, cex = 0.2, add = TRUE)
-    map("state", lwd=1, add =TRUE)
-    map("county", lwd=0.5, lty=3, add =TRUE)
-    mtext(data_per_day$Date[1], side =3, cex = 1, font = 2, line = 1)
+    idw_cal_r_m <- mask(idw_cal_r, california, inverse = FALSE)
+    rasters <- stack(rasters, idw_cal_r_m)
+    #plot(idw_cal_r_m, axes=TRUE)
+    #plot(sp_poly_df, add = TRUE)
+    #plot(spdf_crop, pch=19, cex = 0.2, add = TRUE)
+    #map("state", lwd=1, add =TRUE)
+    #map("county", lwd=0.5, lty=3, add =TRUE)
+    #mtext(data_per_day$Date[1], side =3, cex = 1, font = 2, line = 1)
   }
-  return(idw_cal_r_m)
+  return(rasters)
 }
+rasters <- Interpolate_pollution()
 
-idw_cal_r_m <- Interpolate_pollution()
 
+mapcolor <- colorBin(palette = c('green','yellow', 'red'), domain = c(0:35), bins = 50, pretty = FALSE, na.color = NA)
+legendcolor <- colorBin(palette = c('green', 'yellow', 'red'), domain = c(0:35),  bins =8, pretty = FALSE, na.color = NA)
+maxcolor <- colorBin(palette = c('red'), domain = c(35:110),  bins =2, pretty = FALSE, na.color = NA)
 
-### Plot in Shiny webinterface using leaflet
-library(shiny)
-
-#Prepare UI
-ui <- pageWithSidebar(
+ui <- shinyUI(fluidPage(
   # Application title
-  headerPanel("Air quality during a wildfire event"),
-  # Sidebar with sliders whos sum should be constrained to be 100
-  sidebarPanel(
-    sliderInput("slider1", label = h3("Slider"), min = 0, max = 1, value = 0, step=1),
-    # Create table output
-    mainPanel(map)
-  ))
+  titlePanel("Air quality during a wildfire event"),
+  sidebarLayout(
+    # Sidebar with a slider input
+    sidebarPanel(
+      sliderInput("days", "Period of the 'Sherpa' wildfire", min = 1, max = 82, value = 1, step = 1, pre = 'Day ', animate = TRUE)),
+    # Show a plot of the generated distribution
+    mainPanel(
+      leafletOutput("map")
+    )
+  )
+))
 
-server <- function(input, output, session) {
-  output$value <- renderPrint({ input$slider1 })
+server <- shinyServer(function(input, output){
+  observe({
+    output$map <- renderLeaflet(
+      leaflet() %>% 
+        addProviderTiles("Stamen.TonerLite") %>% 
+        #addTiles() %>%
+        setView(lng=-120, lat = 37, zoom = 6) %>%
+        addLegend(position = "bottomright", pal = legendcolor, values = c(0:35), title = "Legend",labFormat = labelFormat(suffix = ' ug/m³'))
+    )
+    
+  })
+  observe({
+    
+    leafletProxy("map", data = rasters@layers[[input$days]]) %>%
+    #clearGroup(group = "Pollution")%>%
+    addRasterImage(rasters@layers[[input$days]], colors = mapcolor, opacity = 0.5, layerId = input$days, group = "Pollution")  %>%
+    addRasterImage(rasters@layers[[input$days]], colors = maxcolor, opacity = 1)  %>%
+    addLayersControl(baseGroups = c("Open Street Map"),overlayGroups = c("Pollution"), options = layersControlOptions(collapsed = FALSE)) 
+  })
   
-  map = leaflet() %>% addTiles() %>%
-    #addProviderTiles("Stamen.TerrainBackground") %>% 
-    addRasterImage(idw_stack@layers[input$slider1], colors = colorRampPalette(c("yellow", "red"))(length(seq(0, 100, by = 1))-1), opacity = 0.6)
-  
-  output$map = renderLeaflet(map)
-  textOutput
-  
-    observe({#Observer to show Popups on click
+  observe({#Observer to show Popups on click
     click <- input$map_click
     if (!is.null(click)) {
       showpos(x=click$lng, y=click$lat)
     }
   })
-  
   showpos <- function(x=NULL, y=NULL) {#Show popup on clicks
     #Translate Lat-Lon to cell number using the unprojected raster
     #This is because the projected raster is not in degrees, we cannot use it!
-    cell <- cellFromXY(idw_cal_r_m, c(x, y))
+    depth <- projectRasterForLeaflet(rasters@layers[[input$days]])
+    resol <- res(rasters@layers[[input$days]])
+    cell <- cellFromXY(rasters@layers[[input$days]], c(x, y))
+    
     if (!is.na(cell)) {#If the click is inside the raster...
-      xy <- xyFromCell(idw_cal_r_m, cell) #Get the center of the cell
+      xy <- xyFromCell(rasters@layers[[input$days]], cell) #Get the center of the cell
       x <- xy[1]
       y <- xy[2]
       #Get row and column, to print later
-      rc <- rowColFromCell(idw_cal_r_m, cell)
+      rc <- rowColFromCell(rasters@layers[[input$days]], cell)
       #Get value of the given cell
       val = depth[cell]
-      content <- paste0("PM2.5 = ", round(val, 1), " ppm")
+      content <- paste0("PM2.5 = ", round(val, 1), " ug/m³")
       proxy <- leafletProxy("map")
       #add Popup
       proxy %>% clearPopups() %>% addPopups(x, y, popup = content)
@@ -109,96 +124,11 @@ server <- function(input, output, session) {
       proxy %>% clearShapes() %>% addRectangles(x-resol[1]/2, y-resol[2]/2, x+resol[1]/2, y+resol[2]/2)
     }
   }
-}
-
-shinyApp(ui = ui, server = server)
-
-leaflet() %>% addTiles() %>%
-  #addProviderTiles("Stamen.TerrainBackground") %>% 
-  addRasterImage(idw_cal_r_m, colors = colorRampPalette(c("yellow", "red"))(length(seq(0, 100, by = 1))-1), opacity = 0.6)
-
-######################################################################################
-
-# http://rstudio.github.io/shiny/tutorial/#ui-and-server
-
-# Define UI for dataset viewer application
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("Reactivity"),
-  
-  # Sidebar with controls to provide a caption, select a dataset, and 
-  # specify the number of observations to view. Note that changes made
-  # to the caption in the textInput control are updated in the output
-  # area immediately as you type
-  sidebarPanel(
-    textInput("caption", "Caption:", "Data Summary"),
-    
-    selectInput("dataset", "Choose a dataset:", 
-                choices = c("rock", "pressure", "cars")),
-    
-    numericInput("obs", "Number of observations to view:", 10)
-  ),
-  
-  
-  # Show the caption, a summary of the dataset and an HTML table with
-  # the requested number of observations
-  mainPanel(
-    h3(textOutput("caption")), 
-    
-    verbatimTextOutput("summary"), 
-    
-    tableOutput("view")
-  )
-))
-
-library(shiny)
-library(datasets)
-
-# Define server logic required to summarize and view the selected dataset
-shinyServer(function(input, output) {
-  
-  # By declaring datasetInput as a reactive expression we ensure that:
-  #
-  #  1) It is only called when the inputs it depends on changes
-  #  2) The computation and result are shared by all the callers (it 
-  #     only executes a single time)
-  #
-  datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
-  })
-  
-  # The output$caption is computed based on a reactive expression that
-  # returns input$caption. When the user changes the "caption" field:
-  #
-  #  1) This expression is automatically called to recompute the output 
-  #  2) The new caption is pushed back to the browser for re-display
-  # 
-  # Note that because the data-oriented reactive expressions below don't 
-  # depend on input$caption, those expressions are NOT called when 
-  # input$caption changes.
-  output$caption <- renderText({
-    input$caption
-  })
-  
-  # The output$summary depends on the datasetInput reactive expression, 
-  # so will be re-executed whenever datasetInput is invalidated
-  # (i.e. whenever the input$dataset changes)
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
-  
-  # The output$view depends on both the databaseInput reactive expression
-  # and input$obs, so will be re-executed whenever input$dataset or 
-  # input$obs is changed. 
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
-  })
 })
+
+# Complete app with UI and server components
+shinyApp(ui, server)
+
 
 
 
